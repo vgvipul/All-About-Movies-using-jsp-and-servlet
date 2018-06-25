72de3f619949e024c3dd8cbedda10a43850db544
Vipul Gupta <35928346+vgvipul@users.noreply.github.com>
2018-06-25 23:42:07 +0530

    Add files via upload

---
+++ All_About_Movies.sql
@@ -0,0 +1,6 @@
+create database All_About_Movies;
+use All_About_Movies;
+
+create table login( uname varchar(20), pass varchar(20) );
+insert into login values('admin','admin');
+select * from login;
