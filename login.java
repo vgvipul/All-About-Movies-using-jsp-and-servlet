d1cb23c1577612d31b1af9b8b7adbe1eae45baac
Vipul Gupta <35928346+vgvipul@users.noreply.github.com>
2018-06-26 00:10:13 +0530

    lets see

--- login.java
+++ login.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.login.dao.loginDao;
+
 @WebServlet("/login")
 public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -16,8 +18,8 @@ public class login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String uname= request.getParameter("uname");
 		String pass=request.getParameter("pass");
-	   
-		if(uname.equals("admin")&& pass.equals("admin"))
+	   loginDao dao= new loginDao();
+		if(dao.check(uname, pass))
 		{
 			HttpSession session= request.getSession();
 			session.setAttribute("username", uname);
